c438e88d5e16d87cac3c26371ea6c9e8c05f6fc3
Xin Liu <xliu159@hawk.iit.edu>
2018-01-19 15:10:12 -0600

    update latest version

---
+++ myInsAddChat3/myInstagram/TabViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TabViewController.swift
+//  myInstagram
+//
+//  Created by XIN LIU on 1/18/18.
+//  Copyright © 2018 XIN LIU. All rights reserved.
+//
+
+import UIKit
+
+class TabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
